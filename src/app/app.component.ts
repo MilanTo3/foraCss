e4ba8baae754fdd507dff8c609132d7105f13569
Milan Tomin <61879338+MilanTo3@users.noreply.github.com>
2022-09-29 12:44:03 +0200

    foraCss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foraCss';
+
+  ngAfterViewInit() {
+
+    let signUpButton = document.getElementById('signUp')!;
+    let signInButton = document.getElementById('signIn')!;
+    let container = document.getElementById('container')!;
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+  
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+  }
+
 }
